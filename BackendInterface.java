9782ff25e83bb52ff1d3cc63b5c619063306820f
arnie12a <38790024+arnie12a@users.noreply.github.com>
2021-03-22 20:35:56 -0500

    Update BackendInterface.java

--- BackendInterface.java
+++ BackendInterface.java
@@ -12,9 +12,9 @@ import java.util.List;
 
 public interface BackendInterface {
   
-  public void addPokemon(Pokemon pokemon);
-  public Pokemon findPokemonCP(int cp);
-  public List<Pokemon> getPokemonType(String type);
-  public List<Pokemon> getPokemonCPRange(int cp1, int cp2);
-  public List<Pokemon> getPokemonRegion(String region);
+  public void addPokemon(PokemonInterface pokemon);
+  public PokemonInterface findPokemonCP(int cp);
+  public List<PokemonInterface> getPokemonType(String type);
+  public List<PokemonInterface> getPokemonCPRange(int cp1, int cp2);
+  public List<PokemonInterface> getPokemonRegion(String region);
 }

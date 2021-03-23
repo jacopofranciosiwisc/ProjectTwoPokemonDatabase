788595714cebc8555987804b048198e2ed6c1444
arnie12a <38790024+arnie12a@users.noreply.github.com>
2021-03-22 20:35:31 -0500

    Update Backend.java

--- Backend.java
+++ Backend.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
-
-
 public class Backend implements BackendInterface {
-  private RedBlackTree<Pokemon> _tree; // Red Black Tree containing all Pokemon
+  
+  private RedBlackTree<PokemonInterface> _tree; // Red Black Tree containing all Pokemon
   
   /**
    * Constructor that instantiates the backend object and the red black tree as well
    */
   public Backend() {
-    _tree = new RedBlackTree<Pokemon>();
+    _tree = new RedBlackTree<PokemonInterface>();
   }
   
   /**
@@ -23,7 +22,7 @@ public class Backend implements BackendInterface {
    * Allows the front end to add another pokemon into the red black tree
    */
   @Override
-  public void addPokemon(Pokemon pokemon) {
+  public void addPokemon(PokemonInterface pokemon) {
     // TODO Auto-generated method stub
     _tree.insert(pokemon);
   }
@@ -33,30 +32,28 @@ public class Backend implements BackendInterface {
    * @return the Pokemon with the nearest CP value
    */
   @Override
-  public Pokemon findPokemonCP(int cp) {
+  public PokemonInterface findPokemonCP(int cp) {
     // TODO Auto-generated method stub
-    return lookupHelper(cp, _tree.root);
+    if(_tree == null) throw new NullPointerException(
+          "This RedBlackTree cannot store null references.");
+    return getPokemonHelper(cp, _tree.root);
   }
- 
-  /**
-   * Helper method for findPokemonCP
-   * @param cp
-   * @param current
-   * @return
-   */
-  public static Pokemon lookupHelper(int cp, Node<Pokemon> current) {
-    while(current != null) {
-      if(cp < current.getCP()) {
-        current = current.getLeftChild();
-      }
-      else if(cp > current.getCP()) {
-        current = current.getRightChild();
-      }
-      else {
-        return current;
+  
+  private PokemonInterface getPokemonHelper(int val, Node<PokemonInterface> subtree) {
+    if(subtree == null) {
+      return subtree.parent;
+    }
+    else {
+      int compare = val.compareTo(subtree.data);
+      if(compare < 0) {
+        return getPokemonHelper(val, subtree.leftChild);
+      } else if(compare > 0) {
+        return getPokemonHelper(val, subtree.rightChild);
+      } else {
+        // pokemon was found with specific cp value
+        return subtree;
       }
     }
-    return current.parent;
   }
   
   /**
@@ -64,12 +61,12 @@ public class Backend implements BackendInterface {
    * @return list of Pokemon with the selected type
    */
   @Override
-  public List<Pokemon> getPokemonType(String type) {
+  public List<PokemonInterface> getPokemonType(String type) {
     // TODO Auto-generated method stub
-    ArrayList<Pokemon> list_types = new ArrayList<Pokemon>();
-    Iterator<Pokemon> treeIterator = _tree.iterator();
+    ArrayList<PokemonInterface> list_types = new ArrayList<PokemonInterface>();
+    Iterator<PokemonInterface> treeIterator = _tree.iterator();
     while(treeIterator.hasNext()) {
-      Pokemon next = treeIterator.next();
+      PokemonInterface next = treeIterator.next();
       for(int i=0;i<next.getTypes().length;i++) {
         if(type.contains(next.getTypes()[i])) {
           list_types.add(next);
@@ -86,12 +83,12 @@ public class Backend implements BackendInterface {
    *         range that is selected by the user
    */
   @Override
-  public List<Pokemon> getPokemonCPRange(int cp1, int cp2) {
+  public List<PokemonInterface> getPokemonCPRange(int cp1, int cp2) {
     // TODO Auto-generated method stub
-    ArrayList<Pokemon> list_pokemonInCPRange = new ArrayList<Pokemon>();
-    Iterator<Pokemon> treeIterator = _tree.iterator();
+    ArrayList<PokemonInterface> list_pokemonInCPRange = new ArrayList<Pokemon>();
+    Iterator<PokemonInterface> treeIterator = _tree.iterator();
     while(treeIterator.hasNext()) {
-      Pokemon next = treeIterator.next();
+      PokemonInterface next = treeIterator.next();
       if(next.getCP() > cp1 && next.getCP() < cp2) {
         list_pokemonInCPRange.add(next);
       }
@@ -105,12 +102,12 @@ public class Backend implements BackendInterface {
    * @return a list of Pokemon that are from the specific region specified by the user
    */
   @Override
-  public List<Pokemon> getPokemonRegion(String region) {
+  public List<PokemonInterface> getPokemonRegion(String region) {
     // TODO Auto-generated method stub
-    ArrayList<Pokemon> list_regions = new ArrayList<Pokemon>();
-    Iterator<Pokemon> treeIterator = _tree.iterator();
+    ArrayList<PokemonInterface> list_regions = new ArrayList<PokemonInterface>();
+    Iterator<PokemonInterface> treeIterator = _tree.iterator();
     while(treeIterator.hasNext()) {
-      Pokemon next = treeIterator.next();
+      PokemonInterface next = treeIterator.next();
       if(region.contains(next.getRegion().toLowerCase().trim())) {
         list_regions.add(next);
       }

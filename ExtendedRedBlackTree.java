b0c96ba31eb50e2d25ad90a3d71052464be19cf7
arnie12a <38790024+arnie12a@users.noreply.github.com>
2021-03-23 21:15:24 -0500

    Update ExtendedRedBlackTree.java

--- ExtendedRedBlackTree.java
+++ ExtendedRedBlackTree.java
@@ -7,36 +7,54 @@
 // Lecturer: Gary Dahl
 // Notes to Grader: None
 
+/**
+ * ExtendedRedBlackTree extends the RedBlack Tree class by adding a search method
+ * @author arnav
+ *
+ * @param <T>
+ */
 public class ExtendedRedBlackTree<T> extends RedBlackTree<PokemonInterface>{
-  public ExtendedRedBlackTree() {
-    super();
-  }
-  public PokemonInterface getNode(int cp) {
-    return getNodeHelper(cp, root);
-  }
-  public PokemonInterface getNodeHelper(int cp, Node<PokemonInterface> current) {
-    if(current != null) {
-      if(cp > current.data.getCP()) {
-        if(current.rightChild != null) {
-          return getNodeHelper(cp, current.rightChild);
-        } else {
-          // if the pokemon in the red black tree do not have a cp value provided
-          return current.data;
-        }
-      } else if(cp < current.data.getCP()) {
-        if(current.leftChild != null) {
-          return getNodeHelper(cp, current.leftChild);
-        } else {
-          // if the pokemon in the red black tree do not have a cp value provided
-          return current.data;
-        }
-      }
-      else {
-        // when you find the pokemon with the cp value
-        return current.data;
-      }
+    public ExtendedRedBlackTree() {
+        super();
     }
-    return null;
-  }
+    
+    /**
+     * Search method that searches red black tree for given cp
+     * @param cp
+     * @return
+     */
+    public PokemonInterface getNode(int cp) {
+        return getNodeHelper(cp, root);
+    }
+    
+    /**
+     * Helper method to find a node
+     * @param cp
+     * @param current
+     * @return
+     */
+    public PokemonInterface getNodeHelper(int cp, Node<PokemonInterface> current) {
+        if(current != null) { // makes sure the current node is not null
+            if(cp > current.data.getCP()) {
+                if(current.rightChild != null) {
+                    return getNodeHelper(cp, current.rightChild);
+                } else {
+                    // if the pokemon in the red black tree do not have a cp value provided
+                    return current.data;
+                }
+            } else if(cp < current.data.getCP()) {
+                if(current.leftChild != null) {
+                    return getNodeHelper(cp, current.leftChild);
+                } else {
+                    // if the pokemon in the red black tree do not have a cp value provided
+                    return current.data;
+                }
+            }
+            else {
+                // when you find the pokemon with the cp value
+                return current.data;
+            }
+        }
+        return null; // if the root is null
+    }
 }
-
